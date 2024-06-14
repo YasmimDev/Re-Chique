0e6535a96c31871fa66b2343466323e9c2819a1a
Yasmim Victoria <front13@redepos.com.br>
2024-06-14 04:13:19 -0300

    ajsus

--- Carrinho/script/carrinho.js
+++ Carrinho/script/carrinho.js
@@ -3,17 +3,16 @@ const productCheckboxes = document.querySelectorAll('.product-checkbox');
 
 selectAllCheckbox.addEventListener('change', function() {
   productCheckboxes.forEach(checkbox => {
-    checkbox.checked = this.checked; // Set the checked state of product checkboxes to match the "Select All" checkbox
+    checkbox.checked = this.checked; 
   });
 });
 
 productCheckboxes.forEach(checkbox => {
   checkbox.addEventListener('change', function() {
-    // Update the "Select All" checkbox state based on the individual product checkboxes
     selectAllCheckbox.checked = productCheckboxes.every(checkbox => checkbox.checked);
   });
 });
 
 deleteButtons.forEach(button => {
-  // ... (same delete button functionality as before)
+ 
 });
